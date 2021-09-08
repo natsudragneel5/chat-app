3939f615725bc1998e4333be8d8713e4cc61cd8f
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-08 17:11:03 +0530

    initilalizied firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyCaE9HO7W3Azn4xyglJt0rXjBa7icSrocU',
+  authDomain: 'chat-web-app-c70cf.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-c70cf-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-web-app-c70cf',
+  storageBucket: 'chat-web-app-c70cf.appspot.com',
+  messagingSenderId: '1098018585536',
+  appId: '1:1098018585536:web:9a9bb6fcafdbbe90132625',
+};
+
+firebase.initializeApp(config);
