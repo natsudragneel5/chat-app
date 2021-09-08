01714dcab2f60261c53061b8b46d80e9a7099ce7
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-08 21:13:47 +0530

    added signin page and authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import 'rsuite/dist/styles/rsuite-default.css';
-import './styles/main.scss';
+import React from "react";
+import { Switch } from "react-router";
+import "rsuite/dist/styles/rsuite-default.css";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import "./styles/main.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,13 +1,23 @@
-import firebase from 'firebase/app';
-const config = {
-  apiKey: 'AIzaSyCaE9HO7W3Azn4xyglJt0rXjBa7icSrocU',
-  authDomain: 'chat-web-app-c70cf.firebaseapp.com',
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaE9HO7W3Azn4xyglJt0rXjBa7icSrocU",
+  authDomain: "chat-web-app-c70cf.firebaseapp.com",
   databaseURL:
-    'https://chat-web-app-c70cf-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'chat-web-app-c70cf',
-  storageBucket: 'chat-web-app-c70cf.appspot.com',
-  messagingSenderId: '1098018585536',
-  appId: '1:1098018585536:web:9a9bb6fcafdbbe90132625',
+    "https://chat-web-app-c70cf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-web-app-c70cf",
+  storageBucket: "chat-web-app-c70cf.appspot.com",
+  messagingSenderId: "1098018585536",
+  appId: "1:1098018585536:web:9a9bb6fcafdbbe90132625",
 };
 
-firebase.initializeApp(config);
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const database = app.database();
