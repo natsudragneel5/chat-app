2ff2803775b0117d6da99e7b5d629dd4af9cdef6
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-11 11:51:05 +0530

    added buttons to connect and disconnect fb and G

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Drawer, Button, Divider } from "rsuite";
+import { Drawer, Button, Divider, Alert } from "rsuite";
 import { useProfile } from "../../context/profile.context";
+import { database } from "../../misc/firebase";
 import EditableInput from "../EditableInput";
+import ProviderBlock from "./ProviderBlock";
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
   const onSave = async (newData) => {
+    const userNicknameRef = database
+      .ref(`/profile/${profile.uid}`)
+      .child("name");
+    try {
+      await userNicknameRef.set(newData);
+      Alert.success("Nickname has been updated", 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
     console.log(newData);
   };
   return (
@@ -15,6 +26,7 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <ProviderBlock />
         <Divider />
         <EditableInput
           name="nickname"
