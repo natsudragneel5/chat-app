903c33792db8fc3a1353532dddee40bd3712c293
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-13 17:50:50 +0530

    created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useParams } from "react-router";
+import { useRooms } from "../../context/rooms.context";
+import { Loader } from "rsuite";
+import ChatTop from "../../components/chat-window/top/index";
+import Messages from "../../components/chat-window/messages/index";
+import ChatBottom from "../../components/chat-window/bottom/index";
 
 const Chat = () => {
-  return <div>chat page</div>;
+  const { chatId } = useParams();
+  const rooms = useRooms();
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+  const currentRoom = rooms.find((room) => room.id === chatId);
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+  return (
+    <div>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,9 +14,12 @@ const Home = () => {
     <RoomsProvider>
       <Grid fluid className="h-100">
         <Row className="h-100">
-          <Col xs={24} md={8} className="h-100">
-            <Sidebar></Sidebar>
-          </Col>
+          {canRenderSidebar && (
+            <Col xs={24} md={8} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+
           <Switch>
             <Route exact path="/chat/:chatId">
               <Col xs={24} md={16} className="h-100">
