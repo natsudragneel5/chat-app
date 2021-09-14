4a54fb7539bb6ba4736f5844425be888fa01cf76
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-14 16:29:02 +0530

    created modal window for room description

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,9 +1,38 @@
 import React, { memo } from "react";
+import { ButtonToolbar, Icon } from "rsuite";
+import { Link } from "react-router-dom";
 import { useCurrentRoom } from "../../../context/curren-room.context";
+import { useMediaQuery } from "../../../misc/custom-hooks";
+import RoomInfoBtnModal from "./RoomInfoBtnModal";
 
 const ChatTop = () => {
   const name = useCurrentRoom((v) => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery(`(max-width: 992px)`);
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? "d-inline-block p-0 mr-2 text-blue link-unstyled"
+                : "d-none"
+            }
+          />
+          <span className="text-dissappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap"></ButtonToolbar>
+      </div>
+      <div className="d-flex justify-context-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(ChatTop);
