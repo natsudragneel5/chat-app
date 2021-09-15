e0b4cd7faaf5235df5df46887de24eb1154871bc
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-15 12:07:51 +0530

    displayed real time pressence

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TimeAgo from "timeago-react";
+import PresenceDot from "../../PresenceDot";
 import ProfileAvatar from "../../ProfileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
 const MessageItem = ({ message }) => {
@@ -7,6 +8,7 @@ const MessageItem = ({ message }) => {
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-weight-bolder mb-1">
+        <PresenceDot uid={author.uid} />
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
